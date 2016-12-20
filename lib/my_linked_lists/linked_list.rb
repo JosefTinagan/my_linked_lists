0e44275f8cf6ba72ae5350e0797f1943cd595593
Josef Perry Tinagan <josephperry_06@yahoo.com>
2016-12-20 19:57:27 +0800

    added append method and finished its tests

--- lib/my_linked_lists/linked_list.rb
+++ lib/my_linked_lists/linked_list.rb
@@ -1,9 +1,20 @@
 module MyLinkedLists
 	class LinkedList
 		attr_accessor :head, :tail
-		def initialize()
+		def initialize
 			@head = nil
-			@tail = nil
+		end
+
+		def append(item)
+			if head == nil
+				@head = Node.new(item)
+			else
+				tmp = head
+				while tmp.next_node != nil
+					tmp = tmp.next_node
+				end
+				tmp.next_node = Node.new(item,nil)
+			end
 		end
 	end
 end
--- spec/spec_linked_list.rb
+++ spec/spec_linked_list.rb
@@ -3,16 +3,34 @@ require 'spec_helper'
 module MyLinkedLists
 	describe LinkedList do
 		context "#initialize" do
-			it "initializes head and tail with nil" do
+			it "initializes head with nil" do
 				ll = LinkedList.new
 				expect(ll.head).to eq nil
 			end
 
-			it "can change value of head and tail" do
+			it "can change value of head" do
 				ll = LinkedList.new
 				ll.head = "blue"
 				expect(ll.head).to eq "blue"
 			end
 		end
+
+		context "#append" do
+			it "adds a new node when head is nil" do
+				ll = LinkedList.new
+				ll.append("boom")
+				expect(ll.head.value).to eq "boom"
+				expect(ll.head.next_node).to eq nil
+			end
+
+			it "adds the node in the end of list" do
+				ll = LinkedList.new
+				ll.append("first item")
+				ll.append("second item")
+				expect(ll.head.value).to eq "first item"
+				expect(ll.head.next_node.value).to eq "second item"
+			end
+		end
+
 	end
 end
